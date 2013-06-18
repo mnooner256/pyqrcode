4114ffa7ad7ab37120ee15cb317f9239ba615489
Michael Nooner <mnooner256@gmail.com>
2013-06-18 13:13:24 -0500

    Changed the name fo the distribution script so I would not confuse it with the distribute package.

---
+++ topypi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+#This script updates the libraries PIPY page and
+#uploads the code in tar.gz and .zip formats.
+
+python setup.py register
+python setup.py sdist --formats=gztar,zip upload
+
+pushd docs
+make html
+
+pushd _build/html/
+zip -r ../../../docs.zip *
+popd
+popd
+
+echo
+echo "Be sure to change the version on PyPi's website!!!"
+echo "Also, reupload the documentation zip file!"
+echo
