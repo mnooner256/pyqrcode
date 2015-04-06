95776148029db877439edef297e58f1e9dfc94b3
Lars <heuer@semagia.com>
2015-04-06 10:36:11 +0200

    Initial test cases to compare the output of PyQRCode.png against reference images.

--- tests/test_png.py
+++ tests/test_png.py
@@ -2,8 +2,15 @@
 """\
 PNG related tests.
 """
+from __future__ import unicode_literals
+import os
+import io
 from nose.tools import eq_
 import pyqrcode
+try:
+    import png
+except ImportError:
+    from pyqrcode import png
 
 
 def test_size():
@@ -19,6 +26,69 @@ def test_size():
     eq_((qr_size + 2 * border) * 4, code.get_png_size(4))
     border = 0
     eq_((qr_size + 2 * border) * 4, code.get_png_size(4, border=border))
+
+
+_REF_DATA = (
+    # Input string, error level, encoding, reference file
+    ('Märchenbuch', 'M', 'iso-8859-1', 'mb_latin1_m.png'),
+    ('Märchenbuch', 'M', 'utf-8', 'mb_utf8_m.png'),
+)
+
+
+def _make_pixel_array(pixels, greyscale):
+    """\
+    Returns a list of lists. Each list contains 0 and/or 1.
+    0 == black, 1 == white.
+
+    `greyscale`
+        Indiciates if this function must convert RGB colors into black/white.
+    """
+    def bw_color(r, g, b):
+        rgb = r, g, b
+        if rgb == (0, 0, 0):
+            return 0
+        elif rgb == (255, 255, 255):
+            return 1
+        else:
+            raise ValueError('Unexpected RGB tuple %r' % rgb)
+    res = []
+    if greyscale:
+        for row in pixels:
+            res.append(list(row[:]))
+    else:
+        for row in pixels:
+            it = [iter(row)] * 3
+            res.append([bw_color(r, g, b) for r, g, b in zip(*it)])
+    return res
+
+
+def test_write_png():
+    def check(qr, reference):
+        scale, border = 6, 4
+        reader = png.Reader(filename=os.path.join(os.path.dirname(__file__), 'ref/{}'.format(reference)))
+        # Read reference image
+        ref_width, ref_height, ref_pixels, meta = reader.asDirect()
+        ref_pixels = _make_pixel_array(ref_pixels, meta['greyscale'])
+        # Create our image
+        out = io.BytesIO()
+        qr.png(out, scale=scale, border=border)
+        # Excpected width/height
+        expected_width = qr.get_png_size(scale, border)
+        out.seek(0)
+        # Read created image
+        reader = png.Reader(file=out)
+        width, height, pixels, meta = reader.asDirect()
+        pixels = _make_pixel_array(pixels, meta['greyscale'])
+        eq_(expected_width, ref_width)
+        eq_(expected_width, ref_height)
+        eq_(ref_width, width)
+        eq_(ref_height, width)
+        eq_(len(ref_pixels), len(pixels))
+        eq_(ref_pixels, pixels)
+
+    for s, err, encoding, ref in _REF_DATA:
+        qr = pyqrcode.create(s, error=err, encoding=encoding)
+        yield check, qr, ref
 
 
 if __name__ == '__main__':
