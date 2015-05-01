bbe1d6207ddbf4e086a6128e92460d5e60bc445d
Michael Nooner <mnooner256@gmail.com>
2015-05-01 08:14:11 -0500

    Cleans up a intermediate directory for setup.

--- topypi.sh
+++ topypi.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
+#Clean up any .py and pycache files
+for 
+
 #This script updates the libraries PIPY page and
 #uploads the code in tar.gz and .zip formats.
-
 python setup.py register
 python setup.py sdist --formats=gztar,zip upload
 
@@ -18,3 +20,6 @@ echo
 echo "Be sure to change the version on PyPi's website!!!"
 echo "Also, reupload the documentation zip file!"
 echo
+
+#Clean up the egg info directory
+rm -rf *egg-info
