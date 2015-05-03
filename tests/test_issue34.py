deaf1ebece67cfab116fc4b9778c563620a2c0fe
Lars <heuer@semagia.com>
2015-05-03 20:31:30 +0200

    Test cases against issue #34

---
+++ tests/test_issue34.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""\
+Tests against <https://github.com/mnooner256/pyqrcode/issues/34>
+"""
+from __future__ import unicode_literals
+from nose.tools import eq_
+import pyqrcode
+
+
+def test_default_encoding():
+    qr = pyqrcode.create('Märchenbücher', error='m')
+    eq_(1, qr.version)
+    eq_('binary', qr.mode)
+
+
+def test_encoding_latin1():
+    qr = pyqrcode.create('Märchenbücher', error='m', encoding='latin1')
+    eq_(1, qr.version)
+    eq_('binary', qr.mode)
+
+
+def test_encoding_utf8():
+    qr = pyqrcode.create('Märchenbücher', error='m', encoding='utf-8')
+    eq_(2, qr.version)
+    eq_('binary', qr.mode)
+
+
+if __name__ == '__main__':
+    import nose
+    nose.core.runmodule()
