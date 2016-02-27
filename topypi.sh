3aa5e107d2993df228f935ea6fbc066ee94726e0
Michael Nooner <mnooner256@gmail.com>
2016-02-26 19:58:12 -0500

    Updating to version 1.1.1 for distribution.

--- topypi.sh
+++ topypi.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -e
 
-#Clean up any .py and pycache files
-for 
+#Remove pycache and pyc files
+if [ -d .tox ] ; then
+  rm -rf .tox
+fi
+find . -name '__pycache__' -type d | xargs rm -rf
+find . -name '*.pyc' | xargs rm -f
+
 
 #This script updates the libraries PIPY page and
 #uploads the code in tar.gz and .zip formats.
+
 python setup.py register
 python setup.py sdist --formats=gztar,zip upload
 
@@ -20,6 +26,3 @@ echo
 echo "Be sure to change the version on PyPi's website!!!"
 echo "Also, reupload the documentation zip file!"
 echo
-
-#Clean up the egg info directory
-rm -rf *egg-info
