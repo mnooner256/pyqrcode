9a0588140a2796983130a0321219e30df8f2fa3d
Michael Nooner <mnooner256@gmail.com>
2016-05-14 22:42:56 -0400

    Cleaned up the publication script.

--- topypi.sh
+++ topypi.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -e
 
-#Remove pycache and pyc files
+#Remove tox cache, pycache, and pyc files
 if [ -d .tox ] ; then
   rm -rf .tox
 fi
 find . -name '__pycache__' -type d | xargs rm -rf
 find . -name '*.pyc' | xargs rm -f
 
-
-#This script updates the libraries PIPY page and
-#uploads the code in tar.gz and .zip formats.
-
-python setup.py register
-python setup.py sdist --formats=gztar,zip upload
-
+#Build documentation
 pushd docs
 make html
 
+#Create the documentation zip file
 pushd _build/html/
 zip -r ../../../docs.zip *
 popd
 popd
+
+#This script updates the library's PIPY page and
+#uploads the code in tar.gz and .zip formats.
+python setup.py register
+python setup.py sdist --formats=gztar,zip upload
 
 echo
 echo "Be sure to change the version on PyPi's website!!!"
