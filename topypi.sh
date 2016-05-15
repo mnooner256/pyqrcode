f53f43fbda4300aaa482b295f0a7170032dda2cc
Michael Nooner <mnooner256@gmail.com>
2016-05-15 05:24:22 -0400

    Now uploads documentation automatically.

--- topypi.sh
+++ topypi.sh
@@ -10,19 +10,12 @@ find . -name '*.pyc' | xargs rm -f
 #Build documentation
 pushd docs
 make html
-
-#Create the documentation zip file
-pushd _build/html/
-zip -r ../../../docs.zip *
-popd
 popd
 
 #This script updates the library's PIPY page and
 #uploads the code in tar.gz and .zip formats.
 python setup.py register
 python setup.py sdist --formats=gztar,zip upload
+python setup.py upload_docs --upload-dir docs/_build/html
 
-echo
-echo "Be sure to change the version on PyPi's website!!!"
-echo "Also, reupload the documentation zip file!"
-echo
+echo "Done"
