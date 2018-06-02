8123e0f513b0c65f458f437fb1f7a2db0670988d
Lars <heuer@semagia.com>
2018-06-02 22:35:33 +0200

    Renamed test case (referring to the correct issue)

--- tests/test_issue51.py
+++ tests/test_issue51.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """\
-Tests against <https://github.com/mnooner256/pyqrcode/issues/50>
+Tests against <https://github.com/mnooner256/pyqrcode/issues/51>
 """
 from __future__ import unicode_literals
 from nose.tools import eq_
@@ -8,21 +8,21 @@ import pyqrcode
 
 
 class FakeString(str):
-    '''
+    """
     Create a mock class that *acts* like a string as far as needed for the
     QRCode constructor, but raises an exception in case shiftjis encoding is
     used on its value.
 
     This mimics the behaviour of Python on an environment where this codec is
     not installed.
-    '''
+    """
     def __new__(cls, *more):
         return str.__new__(cls, *more)
 
-    def encode(self, encoding=None, errors=None):
+    def encode(self, encoding=None, errors='strict'):
         if encoding == 'shiftjis':
             raise LookupError("unknown encoding: shiftjis")
-        return self.encode(encoding, errors)
+        return super(FakeString, self).encode(encoding, errors)
 
 
 def test_constructing_without_shiftjis_encoding_available():
