2f70e0b54f198e603050347f83b7bdec449ed824
Lars <heuer@semagia.com>
2018-06-03 11:13:40 +0200

    PEP 8

--- pyqrcode/qrspecial.py
+++ pyqrcode/qrspecial.py
@@ -32,7 +32,6 @@ from __future__ import absolute_import, unicode_literals, print_function
 import warnings
 
 
-# ======================================================================
 class QrSpecial(object):
     """
     Special-purpose text for QR codes.
