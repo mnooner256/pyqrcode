4a6a2f9a2d541dfdeeab0bf00ebd43f1a82ac7e0
Lars <heuer@semagia.com>
2018-09-10 00:54:20 +0200

    Added test case against <https://github.com/mnooner256/pyqrcode/issues/17#issuecomment-419731805> and <https://github.com/heuer/pyqrcode/issues/10>

---
+++ tests/test_issue10_17.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""\
+Tests against <https://github.com/mnooner256/pyqrcode/issues/17> and
+<https://github.com/heuer/pyqrcode/issues/10>
+
+Unicode issues.
+"""
+from __future__ import unicode_literals
+import pyqrcode
+
+
+def test_issue_10_17():
+    qr = pyqrcode.create('John’s Pizza')
+    assert qr
+    assert 'binary' == qr.mode
+
+
+if __name__ == '__main__':
+    import pytest
+    pytest.main([__file__])
